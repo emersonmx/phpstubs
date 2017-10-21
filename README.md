bb3147c2ef34e63cba78f9b3117885f5a838273a
Emerson MX <emersonmx@gmail.com>
2017-10-21 01:57:38 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# phpstubs
+
+Repositório com arquivos de stubs para PHP do Netbeans 8.2-201609300101-php.
+
+Execute o `build.sh` e as tags serão geradas :)
---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+filename='netbeans-8.2-201609300101-php.zip'
+checksum='51ae034b99f16c3bab66ab7dfd07c3554953e78828c2493cde1bb67c95c43e2b'
+download_path="/tmp/$filename"
+code_path='code'
+
+pushd /tmp > /dev/null
+curl -O -C - "http://download.netbeans.org/netbeans/8.2/final/zip/$filename"
+popd > /dev/null
+
+echo 'Checking...'
+echo "$checksum *$download_path" | sha256sum -c - > /dev/null
+
+echo 'Extracting...'
+rm -rf $code_path
+unzip -q -j $download_path "netbeans/php/phpstubs/phpruntime/*" -d $code_path/
+
+echo 'Generating tags...'
+ctags --fields=+aimlS --languages=php $code_path
+
+echo 'Done'
